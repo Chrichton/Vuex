796706dcf8431ccb32d74864107c426e50948451
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-03-15 13:50:54 +0100

    show categories (v-for)

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,6 +2,9 @@
   <div>
     <h1>Create Event, {{ user.name }}</h1>
     <p>This event was created by {{ user.name }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
   </div>
 </template>
 
